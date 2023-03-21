dfd3e394de89e5ed28fa433c2e007e60ccd8270a
Theylon Feitosa <62394541+theylonf@users.noreply.github.com>
2023-03-21 19:33:43 -0300

    Create create-user.sh

---
+++ create-user.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Creating group users"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Creating group users"
+
+useradd theylon -m -s /bin/bash -p $(openssl passwd -crypt Neymar1010) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Neymar1010) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Neymar1010) -G GRP_SEC
+
+echo "Specifying directory permissions"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finish add user script"
